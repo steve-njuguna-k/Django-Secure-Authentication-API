d600541774fc644a63673990679a0a3516ac69d1
Steve Njuguna <stevenjuguna93@gmail.com>
2021-09-27 15:42:23 +0300

    Added Register API & corresponding url

--- src/Accounts/urls.py
+++ src/Accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from Accounts import views
 
 urlpatterns = [
-    path('api/register/', views.RegisterAPI, name = "RegisterAPI")
+    path('api/register/', views.RegisterAPI, name = "RegisterAPI"),
+    path('api/login/', views.LoginAPI, name = "LoginAPI")
 ]
--- src/Accounts/views.py
+++ src/Accounts/views.py
@@ -2,6 +2,8 @@ from .serializers import RegisterSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.exceptions import AuthenticationFailed
 
 # Create your views here.
 @api_view(['POST'])
@@ -12,3 +14,20 @@ def RegisterAPI(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response('Failed To Register User!', status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def LoginAPI(request):
+    username = request.data['username']
+    password = request.data['password']
+
+    user = User.objects.filter(username=username).first()
+
+    if user is None:
+        raise AuthenticationFailed('Incorrect Username!')
+
+    if not user.check_password(password):
+        raise AuthenticationFailed('Incorrect Password!')
+
+    return Response({
+        "message":"Login Successful!"
+    })
