2866e34e0b41102d56775b621378ed31d71cc40d
Steve Njuguna <stevenjuguna93@gmail.com>
2021-09-27 15:26:43 +0300

    Added ID to fields in Register serializer

--- src/Accounts/serializers.py
+++ src/Accounts/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
         extra_kwargs = {
             'password': {
                 'write_only': True
